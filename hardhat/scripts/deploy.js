b6a1cecab41e27d948c51b58e1aac5202d098ff5
Rafik-Antonyan <rafikantonyan105@gmail.com>
2023-06-21 17:55:01 +0400

    2

--- hardhat/scripts/deploy.js
+++ hardhat/scripts/deploy.js
@@ -1,18 +1,25 @@
-const { ethers } = require("hardhat");
+const hre = require("hardhat");
 require("dotenv").config({ path: ".env" });
 
 async function main() {
-  const metadataURL = "QmQBHarz2WFczTjz5GnhjHrbUPDnB48W5BM2v2h6HbE1rZ";
-
-  const lw3PunksContract = await ethers.deployContract("LW3Punks", [
-    metadataURL
-  ]);
+  // URL from where we can extract the metadata for a LW3Punks
+  const metadataURL = "ipfs://Qmbygo38DWF1V8GttM1zy89KzyZTPU2FLUzQtiDvB7q6i5";
+  /*
+  DeployContract in ethers.js is an abstraction used to deploy new smart contracts,
+  so lw3PunksContract here is a factory for instances of our LW3Punks contract.
+  */
+  // here we deploy the contract
+ const lw3PunksContract = await hre.ethers.deployContract("LW3Punks", [
+   metadataURL
+ ]);
 
   await lw3PunksContract.waitForDeployment();
 
+ // print the address of the deployed contract
   console.log("LW3Punks Contract Address:", lw3PunksContract.target);
 }
 
+// Call the main function and catch if there is any error
 main()
   .then(() => process.exit(0))
   .catch((error) => {
